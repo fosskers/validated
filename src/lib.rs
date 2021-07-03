9d63e5c1f614335c939ac4afa345d5db49d15874
Colin Woodbury <colin@fosskers.ca>
2021-07-02 20:54:56 -0700

    Fix repo links

--- src/lib.rs
+++ src/lib.rs
@@ -91,7 +91,7 @@
 //! [cats]: https://typelevel.org/cats/datatypes/validated.html
 
 #![warn(missing_docs)]
-#![doc(html_root_url = "https://docs.rs/validated/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/validated/0.1.1")]
 
 use crate::Validated::{Fail, Good};
 use nonempty::NonEmpty;
